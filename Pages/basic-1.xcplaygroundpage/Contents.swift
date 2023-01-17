a028892758bf15026b1c3c0990529c9d776acd01
= <moonkey48@icloud.com>
2023-01-17 15:42:07 +0900

    add enum uses

--- Pages/basic-1.xcplaygroundpage/Contents.swift
+++ Pages/basic-1.xcplaygroundpage/Contents.swift
@@ -6,3 +6,37 @@ if !isDarkenModeOn {
     print("darke mode off")
 }
 
+var arr : Array<Int> = [0,1,2,3,4,5]
+
+for item in arr {
+    print("item \(item)")
+}
+
+enum School {
+    case elementary, middle, high
+}
+
+let mySchool = School.elementary
+print("mySchool is \(myShool)")
+
+enum Grade: Int {
+    case first = 1
+    case second = 2
+}
+let myGrade = Grade.second
+
+enum ShoolDetail {
+    case elementary(name:String)
+    case middle(name:String)
+
+    func get() -> String {
+        switch self {
+            case .elementary(let name):
+                return name
+            case .middle(let name):
+                return name
+        }
+    }
+}
+let myMiddleSchoolName = SchoolDetail.middle(name: 'jeju school')
+print("middleSchool is \(myMiddleShoolName.get())")
